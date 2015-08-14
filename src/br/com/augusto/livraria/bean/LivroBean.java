355a06b92a278981ad43698f48a5dd30d9b321cd
Augusto Sandim <augusto@jera.com.br>
2015-08-14 00:05:03 -0400

    Conversão e validação de dados

--- src/br/com/augusto/livraria/bean/LivroBean.java
+++ src/br/com/augusto/livraria/bean/LivroBean.java
@@ -2,8 +2,12 @@ package br.com.augusto.livraria.bean;
 
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 
 import br.com.augusto.livraria.dao.DAO;
 import br.com.augusto.livraria.model.Autor;
@@ -14,7 +18,7 @@ import br.com.augusto.livraria.model.Livro;
 public class LivroBean {
 	private Livro livro = new Livro();
 	private Integer autorId;
-	
+
 	public Integer getAutorId() {
 		return autorId;
 	}
@@ -26,25 +30,38 @@ public class LivroBean {
 	public Livro getLivro() {
 		return livro;
 	}
-	
-	public List<Autor> getAutoresDoLivro(){
+
+	public List<Autor> getAutoresDoLivro() {
 		return this.livro.getAutores();
 	}
-	
+
 	public List<Autor> getAutores() {
 		return new DAO<Autor>(Autor.class).listaTodos();
 	}
-	
+
 	public void gravarAutor() {
 		Autor autor = new DAO<Autor>(Autor.class).buscaPorId(this.autorId);
 		this.livro.adicionaAutor(autor);
 	}
-	
+
 	public void gravar() {
 		System.out.println("Gravando livro " + this.livro.getTitulo());
-		if(livro.getAutores().isEmpty()) {
-			throw new RuntimeException("Livro deve conter pelo menos um autor");
+		if (livro.getAutores().isEmpty()) {
+			// throw new
+			// RuntimeException("Livro deve conter pelo menos um autor");
+			FacesContext.getCurrentInstance().addMessage("autor",
+					new FacesMessage("Livro deve conter pelo menos um autor"));
+			return;
 		}
 		new DAO<Livro>(Livro.class).adicionar(livro);
+	}
+
+	public void comecaComDigitoUm(FacesContext fc, UIComponent component,
+			Object value) throws ValidatorException {
+		String valor = value.toString();
+		if (!valor.startsWith("1")) {
+			throw new ValidatorException(new FacesMessage(
+					"Deveria começar com 1"));
+		}
 	}
 }

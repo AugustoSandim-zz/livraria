ee36b3eb6bf90140bec5cc572b804c7ea34bfd45
Augusto Sandim <augusto@jera.com.br>
2015-08-13 23:25:14 -0400

    Completando o sistema e lidando com escopos do JSF 2

--- src/br/com/augusto/livraria/bean/LivroBean.java
+++ src/br/com/augusto/livraria/bean/LivroBean.java
@@ -1,21 +1,50 @@
 package br.com.augusto.livraria.bean;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import br.com.augusto.livraria.dao.DAO;
+import br.com.augusto.livraria.model.Autor;
 import br.com.augusto.livraria.model.Livro;
 
 @ManagedBean
+@ViewScoped
 public class LivroBean {
 	private Livro livro = new Livro();
+	private Integer autorId;
+	
+	public Integer getAutorId() {
+		return autorId;
+	}
+
+	public void setAutorId(Integer autorId) {
+		this.autorId = autorId;
+	}
 
 	public Livro getLivro() {
 		return livro;
 	}
 	
+	public List<Autor> getAutoresDoLivro(){
+		return this.livro.getAutores();
+	}
+	
+	public List<Autor> getAutores() {
+		return new DAO<Autor>(Autor.class).listaTodos();
+	}
+	
+	public void gravarAutor() {
+		Autor autor = new DAO<Autor>(Autor.class).buscaPorId(this.autorId);
+		this.livro.adicionaAutor(autor);
+	}
+	
 	public void gravar() {
 		System.out.println("Gravando livro " + this.livro.getTitulo());
-		
+		if(livro.getAutores().isEmpty()) {
+			throw new RuntimeException("Livro deve conter pelo menos um autor");
+		}
 		new DAO<Livro>(Livro.class).adicionar(livro);
 	}
 }
